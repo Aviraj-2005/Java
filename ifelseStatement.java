a307aa2f7cea7d73b1182b0dd38f8592fa66bfe0
Aviraj-2005 <aviinamdar2005@gmail.com>
2025-05-23 12:07:28 +0530

    if else

---
+++ ifelseStatement.java
@@ -0,0 +1,16 @@
+class ifelseStatement
+{
+    public static void main(String []args)
+    {
+        int a=5;
+        int b=8;
+        if(a>b)
+        {
+            System.out.println(a+" is max");
+        }
+        else
+        {
+            System.out.println(b+ " is max");
+        }
+    }
+}
