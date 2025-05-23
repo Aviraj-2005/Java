f6264bfccbffe3e9e7139f070a07f33c8bb9f88b
Aviraj-2005 <aviinamdar2005@gmail.com>
2025-05-23 11:51:39 +0530

    if

---
+++ ifstate.java
@@ -0,0 +1,12 @@
+class ifstate
+{
+    public static void main(String []args)
+    {
+        int i=3;
+        if(i>=0)
+        {
+            System.out.println("No is Positive :"+i);   // it check only one condition
+        }
+    
+    }
+}
