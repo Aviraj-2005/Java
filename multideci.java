314c61ef3ce4cb00202cfbb535e33788297a2604
Aviraj-2005 <aviinamdar2005@gmail.com>
2025-05-23 12:05:33 +0530

    if else-if else

---
+++ multideci.java
@@ -0,0 +1,19 @@
+class multideci
+{
+    public static void main(String []args)
+    {
+        int a=10;
+        if(a>15)
+        {
+            System.out.println(a+"is greter than 15");
+        }
+        else if(a>5)
+        {
+            System.out.println(a+" is greter than 5");
+        }
+        else
+        {
+            System.out.println(a+"is less than or equal to 5");
+        }
+    }
+}
